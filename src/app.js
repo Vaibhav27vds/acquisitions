f9adad0e0050cc6df219f9b3be9392d6bf592431
Vaibhav Deep Srivastava <Vaibhav27vds@gmail.com>
2025-10-07 06:49:35 +0530

    setup logging and middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+app.use(cookieParser());
+app.use(cors(
+  {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }
+));
+
+
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisions!')
   res.status(200).send('Hello, World!');
 });
 
